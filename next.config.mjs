72ab91aac42ede15cd2b820a4a38868f5469bc53
kjunyoung676-ops <kjunyoung676@gmail.com>
2026-04-30 15:12:05 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias['canvas'] = false
+    return config
+  },
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        canvas: './canvas-mock.js',
+      },
+    },
+  },
 }
-
 export default nextConfig
